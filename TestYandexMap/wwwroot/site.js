5a28a4e42f3f5ff8d11fad35a074c9e9ee18adc1
farida <dev18@laposte.net>
2025-09-19 13:02:16 +0200

    Добавьте файлы проекта.

---
+++ TestYandexMap/wwwroot/site.js
@@ -0,0 +1,23 @@
+﻿window.map = {
+    start: function (address) {
+        ymaps.ready(init);
+
+        function init() {
+            var myMap = new ymaps.Map("map", {
+                center: [55.76, 37.64],
+                zoom: 10
+            });
+
+            ymaps.geocode(address).then(function (res) {
+                var firstGeoObject = res.geoObjects.get(0);
+                var coords = firstGeoObject.geometry.getCoordinates();
+                var bounds = firstGeoObject.properties.get('boundedBy');
+
+                myMap.geoObjects.add(firstGeoObject);
+                myMap.setBounds(bounds, {
+                    checkZoomRange: true
+                });
+            });
+        }
+    }
+}
