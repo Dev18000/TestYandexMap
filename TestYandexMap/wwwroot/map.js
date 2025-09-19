2522dae01fa394006b24112be05ed571342d822c
farida <dev18@laposte.net>
2025-09-19 15:32:51 +0200

    fitToViewport

--- TestYandexMap/wwwroot/map.js
+++ TestYandexMap/wwwroot/map.js
@@ -6,23 +6,33 @@ window.map = {
         function init() {
             var myMap = new ymaps.Map("map", {
                 center: [55.76, 37.64],
-                zoom: 10,
-                
+                zoom: 10
             });
+
+            myMap.container.fitToViewport();
 
             ymaps.geocode(address).then(function (res) {
                 var firstGeoObject = res.geoObjects.get(0);
-                var coords = firstGeoObject.geometry.getCoordinates();
                 var bounds = firstGeoObject.properties.get('boundedBy');
 
                 myMap.geoObjects.add(firstGeoObject);
                 myMap.setBounds(bounds, {
-                    checkZoomRange: true
+                    checkZoomRange: true,
+                    useMapMargin: true
                 });
+
+                requestAnimationFrame(function () {
+                    myMap.container.fitToViewport();
+                });
+            });
+
+            window.addEventListener("resize", function () {
+                myMap.container.fitToViewport();
             });
         }
     }
 }
+
 window.map.dispose = function () {
     if (window.map._instance) {
         window.map._instance.destroy();
